527199817704757cf6b850e51eeefab0e7d8116c
OzakiSatoshi <60631984+OzakiSatoshi@users.noreply.github.com>
2024-10-05 10:25:49 +0900

    apps.jp up

---
+++ app.js
@@ -0,0 +1,44 @@
+// Cesium viewerの初期化
+const viewer = new Cesium.Viewer('cesiumContainer', {
+    terrainProvider: Cesium.createWorldTerrain()
+});
+
+// Google Maps Platformのクレジットを追加
+viewer.scene.frameState.creditDisplay.addDefaultCredit(
+    new Cesium.Credit('<a href="https://www.google.com/help/terms_maps/">Google</a> © 2023', '', 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png')
+);
+
+// Autocompleteウィジェットの設定
+const input = document.getElementById('pac-input');
+const autocomplete = new google.maps.places.Autocomplete(input);
+
+// 場所が選択されたときのイベントリスナー
+autocomplete.addListener('place_changed', () => {
+    const place = autocomplete.getPlace();
+    if (!place.geometry) return;
+
+    // 選択された場所に移動
+    viewer.camera.flyTo({
+        destination: Cesium.Cartesian3.fromDegrees(
+            place.geometry.location.lng(),
+            place.geometry.location.lat(),
+            1000
+        ),
+        orientation: {
+            heading: Cesium.Math.toRadians(0),
+            pitch: Cesium.Math.toRadians(-45),
+            roll: 0.0
+        }
+    });
+
+    // 選択された場所にマーカーを追加
+    viewer.entities.add({
+        position: Cesium.Cartesian3.fromDegrees(
+            place.geometry.location.lng(),
+            place.geometry.location.lat()
+        ),
+        billboard: {
+            image: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+        }
+    });
+});
